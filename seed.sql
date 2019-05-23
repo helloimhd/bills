2aafd1096479224b5dc6f9d926ff356c825534b5
helloimhd <herda58@gmail.com>
2019-05-23 16:11:33 +0800

    edit seed file

--- seed.sql
+++ seed.sql
@@ -4,9 +4,14 @@ INSERT INTO users (username, password, email) VALUES ('chris', 'chris', 'chris@e
 INSERT INTO users (username, password, email) VALUES ('andrew', 'andrew', 'andrew@email.com' );
 
 
-INSERT INTO receipts (user_id, group_id, img_token, subtotal, total) VALUES (1, 1, 'guQnFRzRY4MXMm6F', null, null );
+INSERT INTO receipts (user_id, group_id, img_token, subtotal, total) VALUES (1, 1, 'guQnFRzRY4MXMm6F', 30, null );
 
 
 
 INSERT INTO groups (receipt_id, friend_id, amount) VALUES (1, 2, null);
 INSERT INTO groups (receipt_id, friend_id, amount) VALUES (1, 3, null);
+
+
+ INSERT INTO items (receipt_id, item_name, price, quantity, users_id) VALUES( 2, "Ice Mocha", 10.00, 1, ARRAY[1,2] )
+ INSERT INTO items (receipt_id, item_name, price, quantity, users_id) VALUES( 2, "Ice Latte", 10.00, 1, ARRAY[1] )
+ INSERT INTO items (receipt_id, item_name, price, quantity, users_id) VALUES( 2, "Ps.Fluffy Pancake", 10.00, 1, ARRAY[3] )
