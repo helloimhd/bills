47bfb8b40825da37bd806ed698225d2819303bee
shiowwj <shiowweijun@gmail.com>
2019-05-24 12:23:24 +0800

    receipt page ajax handler

--- src/client/App.jsx
+++ src/client/App.jsx
@@ -9,17 +9,58 @@ import Selection from './components/itemSelection/item';
 
 
 class App extends React.Component {
-  constructor() {
-    super();
-    // this.state = {
-    //     message: 'hello',
-    // }
-  }
+    constructor() {
+        super();
+        this.state = {
+            receipt: [],
+        }
+
+    }
+
+    getReceiptHandler=()=>{
+        //retrieves receipt and item info
+        var reactThis = this;
+        var img_token = 'guQnFRzRY4MXMm6F';
+        var receipt_id;
+        var obj = {};
+
+        async function getReceipt(token){
+
+            let response = await fetch(`/receipt/${img_token}`);
+            let data = await response.json();
+            return data;
+        }
+
+        async function getItems(id){
+
+            let response = await fetch(`/items/${id}`);
+            let data = await response.json();
+            return data;
+        }
+
+        getReceipt(img_token).then(receiptOutput=> {
+
+            receipt_id = receiptOutput[0].id;
+            getItems(receipt_id).then(itemOutput=>{
+
+                obj =  {
+                    receipt_id: receiptOutput[0].id,
+                    user_id: receiptOutput[0].user_id,
+                    group_id: receiptOutput[0].group_id,
+                    img_token: receiptOutput[0].img_token,
+                    sub_total: receiptOutput[0].sub_total,
+                    items: itemOutput,
+                        }
+
+                this.setState( {receipt: obj} );
+            })
+        })
+    }
 
   render() {
     return (
       <div>
-        <Receipt/>
+        <Receipt getReceiptHandler={this.getReceiptHandler}/>
         <Selection/>
       </div>
     );
--- src/client/components/receipt/receipt.jsx
+++ src/client/components/receipt/receipt.jsx
@@ -1,19 +1,25 @@
 import React from 'react';
 
-// import PropTypes from 'prop-types';
+import PropTypes from 'prop-types';
 
 // import styles from './style.scss';
 
 class Receipt extends React.Component{
     constructor(){
         super();
-    }render(){
+    }
+
+    render(){
         return(
             <div>
-                <p>Shows receipt based off JSON reply. User-main can edit individual items</p>
+                <p >Shows receipt based off JSON reply. User-main can edit individual items</p>
+                <button onClick={()=>{this.props.getReceiptHandler()}}> Next </button>
             </div>
         );
     }
 }
+Receipt.propTypes = {
+  getReceiptHandler: PropTypes.func,
+};
 
 export default Receipt;
