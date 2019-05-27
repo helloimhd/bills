fc764cf572e1900b411d54f84308a9e0cb74669c
shiowwj <shiowweijun@gmail.com>
2019-05-27 15:48:25 +0800

    not working

--- src/client/components/itemSelection/item.jsx
+++ src/client/components/itemSelection/item.jsx
@@ -10,73 +10,85 @@ import styles from './style.scss';
 class IndividualMember extends React.Component{
     constructor(){
         super();
-        this.state={
-            checked:false,
-        }
     }
 
-    blah = (e)=>{
+    checkBoxHandler = (e)=>{
 
-        this.setState( {checked: !this.state.checked} );
-
-        this.checker(!this.state.checked,e.target.value);
+        this.props.takeStatus(e.target.id);
     }
 
-    checker=(status,id)=>{
+    // checker=(status,id)=>{
 
-        if(status){
-            console.log('THIS GUY NEEDS TO PAY!!!!', status);
-        }else{
-            console.log('Nope... not him...', id)
-        }
-    }
+    //     if(status){
+    //         console.log('THIS GUY NEEDS TO PAY!!!!', id , status);
+    //     }else{
+    //         console.log('Nope... not him...', id, status)
+    //     }
+    // }
+
     render(){
+
         return(
             <React.Fragment>
                 <input
                     type="checkbox"
-                    value={this.props.member[0].id}
+                    value={this.props.member[0].username}
+                    id={this.props.member[0].id}
                     ref="input"
-                    onChange={(e)=>{this.blah(e)}}
-                    checked={this.state.checked}/>{this.props.member[0].username}<br/>
-            </React.Fragment>
-
-        );
-    }
-}
-
-class GroupMembers extends React.Component{
-    render(){
-        const members = [];
-        this.props.group.forEach((item,index)=>{
-                members.push(
-                    <IndividualMember
-                        member={item}
-                        key={index}/>
-                )
-        })
-        return(
-            <React.Fragment>
-                {members}
+                    onChange={(e)=>{this.checkBoxHandler(e)}}
+                    checked={this.props.status}
+                    />{this.props.member[0].username}<br/>{this.props.currentItem.item_name}<br/>
             </React.Fragment>
         );
     }
 }
 
 class ItemDetail extends React.Component{
+    constructor(){
+        super()
+    }
+
+    takeStatusFromChild=(id)=>{
+        console.log('yea gon live up', id);
+
+    }
+
     render(){
+        const members = [];
+
+        console.log('start',this.props)
+
+
+
+        this.props.item.group.forEach((item,index)=>{
+
+
+                members.push(
+                    <IndividualMember
+                        currentItem={this.props.item}
+                        member={item}
+                        key={index}
+                        checked={status}
+                        takeStatus={this.takeStatusFromChild}/>
+                )
+        })
         return(
-            <p>## Item --- Strawberry Cake --- $15 </p>
+            <React.Fragment>
+                <p>Qty:{this.props.item.quantity}  {this.props.item.item_name} ${this.props.item.price}</p>
+                {members}
+            </React.Fragment>
         );
     }
 }
+
+// <GroupMembers group={this.props.group}/>
 
 class ButtonTab extends React.Component{
     render(){
         return(
             <React.Fragment>
-                <button>Previous</button>
-                <button>Next</button>
+                <button onClick={()=>{this.props.previousHandler()}}>Previous</button>
+                <button onClick={()=>{this.props.nextHandler()}}>Next</button>
             </React.Fragment>
         );
     }
@@ -85,11 +97,14 @@ class ButtonTab extends React.Component{
 class ItemCard extends React.Component{
 
     render(){
+        const items = this.props.items;
+        const numberNow = this.props.itemNumberNow;
+
+        let giveItemNow = items[numberNow];
+        // console.log(giveItemNow);
         return(
             <React.Fragment>
-                <ItemDetail items={this.props.items}/>
-                <GroupMembers group={this.props.group}/>
-                <ButtonTab/>
+                <ItemDetail item={giveItemNow} number={numberNow}/>
             </React.Fragment>
         );
     }
@@ -98,11 +113,37 @@ class ItemCard extends React.Component{
 class ItemSelection extends React.Component{
     constructor(){
         super();
-    }render(){
-    //this component should keep track of how many items in the receipt
+        this.state = {
+            counter:0,
+        }
+    }
+
+    nextHandler=()=>{
+
+        if(this.state.counter !== this.props.items.length-1){
+            let counterNow = this.state.counter + 1;
+
+            this.setState({counter:counterNow});
+        }
+    }
+
+    previousHandler =()=>{
+
+        if(this.state.counter > 0){
+            let counterNow = this.state.counter - 1;
+
+            this.setState({counter:counterNow});
+        }
+    }
+    render(){
+        //this component should keep track of how many items in the receipt
+        this.props.items.forEach((item,index)=>{
+            item.group = this.props.group;
+        })
         return(
             <div className={styles.itemSelectionContainer}>
-                <ItemCard items={this.props.items} group={this.props.group}/>
+                <ItemCard items={this.props.items} itemNumberNow={this.state.counter}/>
+                <ButtonTab nextHandler={this.nextHandler} previousHandler={this.previousHandler}/>
             </div>
         );
     }
