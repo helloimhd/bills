ddf08513bb26e38b22d3fedd7c8b2ddee58636fd
Herda <47418984+helloimhd@users.noreply.github.com>
2019-05-30 09:40:02 +0800

    change api key

--- src/server/controllers/receipts.js
+++ src/server/controllers/receipts.js
@@ -12,7 +12,7 @@ cloudinary.config({
 });
 
 
-const tabUrl = 'https://api.tabscanner.com/pcVKfMzzwCYEkSUKcXvBWfb3ywzPg46WtUoTs6UsCduGfbYNToC0z6BmK0bvRLnB';
+const tabUrl = 'https://api.tabscanner.com/AcMHx0XLLafK4avM8WdBLhZixu2fRP8WeY0z4rv1RCFPjNALkAnYIuQnJtH2BOqs';
 // pcVKfMzzwCYEkSUKcXvBWfb3ywzPg46WtUoTs6UsCduGfbYNToC0z6BmK0bvRLnB
 // AcMHx0XLLafK4avM8WdBLhZixu2fRP8WeY0z4rv1RCFPjNALkAnYIuQnJtH2BOqs
 
